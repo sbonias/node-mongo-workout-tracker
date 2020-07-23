21733607c518f1e54266ffbfb71f668ea53e7920
Sam Bonias <sbonias84@gmail.com>
2020-07-23 10:28:22 -0400

    setup initial files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,9 @@
+// require express
+const router = require('express').Router();
+// require model
+const Workout = require('../models/workout');
+
+// based on server.js file this is what a route should look like with use of Mongo
+router.post('/api/workout', (req, res) => {
+  Workout.create({}).then();
+});
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+let MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/workout';
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+// routes
+app.use(require('./routes/apiRoutes'));
+app.use(require('./routes/view.js'));
